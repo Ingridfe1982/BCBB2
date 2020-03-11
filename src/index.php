cbfef9e6318e3042e43edf784843499529a29c91
becode <ingrid.feijt@hotmail.com>
2020-03-11 12:08:30 +0100

    added to index.php boards and topics

--- src/index.php
+++ src/index.php
@@ -1,8 +1,76 @@
 <?php
 if (empty(session_id())) {session_start();}
+include_once('_functions.php');
+$db = openDb();
+$req = $db->prepare('SELECT * FROM boards');
+$req->execute(array());
+$boards = $req->fetchAll();
+
+// echo '<pre>' . var_export($boards, true) . '</pre>';die;
 
 include('_header.php');
 include('_nav.php');
+?>
+<table class="table">
+    <tbody>
+        <?php
+        foreach($boards as $board) {
+            echo '
+                <tr class="board">
+                    <th scope="row">'.$board["name"].'</th>
+                </tr>
+            ';
+
+            $queryTopics =
+                'SELECT * 
+                FROM topics t
+                --  LEFT JOIN messages m
+                --  ON m.id_topic = t.id
+                RIGHT JOIN messages m
+                ON t.id = m.id_topic
+                WHERE id_board = :idBoard 
+                ORDER BY m.edition_date DESC
+                LIMIT 3
+                ';
+
+            $reqTopics = $db->prepare($queryTopics);
+            $reqTopics->execute(array(
+                'idBoard'=> $board["id"]
+            ));
+            $topics = $reqTopics->fetchAll();
+
+            foreach($topics as $topic) {
+                echo'
+                    <tr class="topic">
+                        <th scope="row">'.$topic["title"].'</th>
+                    </tr>
+                ';
+            }
+        }
+        ?>
+    </tbody>
+</table>
+<!-- <table class="table">
+  <tbody>
+    <tr class="board">
+      <th scope="row">General</th>
+    </tr>
+    <tr class="topic">
+      <th scope="row">Topic 1</th>
+    </tr>
+    <tr class="board">
+      <th scope="row">Development</th>
+    </tr>
+    <tr class="board">
+      <th scope="row">Smalltalk</th>
+    </tr>
+    <tr class="board">
+      <th scope="row">Events</th>
+    </tr>
+  </tbody>
+</table> -->
+
+<?php
 include('_footer.php');
 ?>
 
